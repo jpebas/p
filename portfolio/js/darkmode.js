3726432785eba7661a831d1deec7ab7dc3ce55e7
jpebas <111980157+jpebas@users.noreply.github.com>
2023-08-18 19:59:35 +0800

    Add files via upload

---
+++ portfolio/js/darkmode.js
@@ -0,0 +1,11 @@
+let icon = document.getElementById('icon');
+icon.onclick = function(){
+    document.body.classList.toggle('dark-theme');
+    if (document.body.classList.contains('dark-theme')){
+        icon.src = 'Images/sun.png';
+    }
+    else {
+        icon.src = 'Images/moon.png';
+    }
+}
+
